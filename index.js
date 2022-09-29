4aa437a0e7980cfab4c62cf55f81629f340f5779
Manomay Kagalkar <manomay111@gmail.com>
2022-09-29 22:13:49 +0530

    home page completed
    time taken - 4 hours

--- index.js
+++ index.js
@@ -7,3 +7,34 @@ const { default: mongoose } = require('mongoose');
 const { doesNotMatch } = require("assert");
 const { createDiffieHellmanGroup } = require("crypto");
 let app = express();
+
+// connecting to MongoDB Atlas
+mongoose.connect(process.env.ATLAS_CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// creating user schema
+let userSchema = new mongoose.Schema({
+    username: {type: String, required: true},
+    password: {type: String, required: true},
+    email: {type: String},
+    isAdmin: {type: Boolean, default: false}
+});
+
+// creating user model
+let User = mongoose.model("User", userSchema);
+
+// initializing server
+app.listen(8080, "localhost");
+
+// middleware
+app.use("/", (req, res, next) => {
+    console.log(req.method + req.ip + req.path);
+    next();
+});
+
+app.use("/resources", express.static(__dirname + "/resources"));
+app.use("/html", express.static(__dirname + "/html"));
+
+// root path request and response
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/html/home.html");
+});
